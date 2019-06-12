a7f601ccf70e63556156121c177eadfab8595968
2zino <2zino@naver.com>
2019-06-12 20:40:41 +0900

    시작

---
+++ src/main/java/com/wb/hb/NpwbApplication.java
@@ -0,0 +1,13 @@
+package com.wb.hb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpwbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpwbApplication.class, args);
+	}
+
+}

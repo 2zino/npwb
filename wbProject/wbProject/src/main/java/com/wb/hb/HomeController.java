259161ecc769d1e843846265704a36ed30079504
jangGiraffe <jangGiraffe@gamil.com>
2019-10-03 13:03:15 +0900

    테스트용커밋

--- wbProject/wbProject/src/main/java/com/wb/hb/HomeController.java
+++ wbProject/wbProject/src/main/java/com/wb/hb/HomeController.java
@@ -1,5 +1,8 @@
 package com.wb.hb;
 
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -10,6 +13,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
 
 /**
  * Handles requests for the application home page.
@@ -32,6 +38,30 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping(value = "/main2.do", method = RequestMethod.GET)
+	public String home2(Locale locale, Model model) throws ClassNotFoundException, SQLException {
+		logger.info("hi");
+		
+		java.sql.Connection con= null;
+		java.sql.Statement stmt = null;
+		ResultSet rset = null;
+		String url="106.10.53.202:3306/mysql";
+		String query=null;
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		con = DriverManager.getConnection(url,"root","1234qwer");
+		stmt = con.createStatement();
+		query="select CONTENTS from mysql.TB_BIBLE where seq = '17772'";
+		rset = stmt.executeQuery(query);
+		
+		while(rset.next()) {
+			System.out.println("ㅇㅇㅇㅇ");
+		}
+		logger.info("bye");
 		
 		return "home";
 	}

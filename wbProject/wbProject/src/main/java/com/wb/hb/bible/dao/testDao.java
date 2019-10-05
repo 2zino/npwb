c52366d2b10f9a700a3251bbe31cb073d6c0eb77
jangGiraffe <jangGiraffe@gmail.com>
2019-10-05 13:43:17 +0900

    쿼리테스트2

--- wbProject/wbProject/src/main/java/com/wb/hb/bible/dao/testDao.java
+++ wbProject/wbProject/src/main/java/com/wb/hb/bible/dao/testDao.java
@@ -5,14 +5,17 @@ import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class testDao {
 
 	@Autowired
+	@Qualifier("sqlSession") //root-context.xml의 org.mybatis.spring.SqlSessionTemplate의 bean id와 해당 객체를 autowired시켜주기위함
 	SqlSession sql;
 	
+	@SuppressWarnings("unchecked")
 	public List<HashMap<String,String>> getGospe() {
 		HashMap<String,String> input = new HashMap<String, String>();
 		input.put("version", "새번역");

37aa42cf6e8d7efa2218f4152bafe0451bad1ba9
jangGiraffe <jangGiraffe@gmail.com>
2019-10-05 15:51:41 +0900

    Repository 어노테이션 추가

--- wbProject/wbProject/src/main/java/com/wb/hb/bible/dao/defaultDao.java
+++ wbProject/wbProject/src/main/java/com/wb/hb/bible/dao/defaultDao.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class defaultDao {
 	@Autowired
 	@Qualifier("sqlSession") //root-context.xml의 org.mybatis.spring.SqlSessionTemplate의 bean id와 해당 객체를 autowired시켜주기위함
